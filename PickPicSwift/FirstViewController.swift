d438de50325c70c6c07dfb2cbeac9e138703a5c1
Neil Edwards <neil@plasticwater.com>
2014-08-19 07:39:47 -0400

    Initial Commit

---
+++ PickPicSwift/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  PickPicSwift
+//
+//  Created by Neil Edwards on 8/19/14.
+//  Copyright (c) 2014 Neil Edwards. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
---
+++ PickPicSwift/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PickPicSwift
+//
+//  Created by Neil Edwards on 8/19/14.
+//  Copyright (c) 2014 Neil Edwards. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+                            
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
